49ae3a11d52bd6373f99ac09e9b603f4495b3e95
siuwhat <1290634672>
2022-03-19 20:20:59 +0800

    c

--- weibo/insertmysql.py
+++ weibo/insertmysql.py
@@ -1,2 +1,18 @@
 import pymysql
-flag=pymysql.connect(host='localhost',passwd='root')
+import csv
+
+reader=csv.reader(open('../text.csv','r',encoding='gb18030'))
+text=list(reader)
+for i in range(1,len(text)):
+    print(text[i][2].split()[0])
+    f=open('../'+text[i][2].split()[0]+'.csv','a',encoding='gb18030',newline='')
+    writer=csv.writer(f)
+    writer.writerow(text[i])
+    f.close()
+
+db=pymysql.connect(host='localhost',passwd='root',user='root',database='spider')
+flag=db.cursor()
+t=flag.execute('select * from test;')
+if t!=0:
+    flag.execute('truncate table test')
+print(t)
