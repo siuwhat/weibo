7eef1708cf48e6a5a570d13f139791cdc4e0e9e0
siuwhat <1290634672>
2022-04-09 14:06:31 +0800

    add k-means algorithm

--- weibo/jieba/kmeans.py
+++ weibo/jieba/kmeans.py
@@ -11,13 +11,13 @@ for i in model.wv.index_to_key:
     newlist.append(model.wv[i])
 # print(newlist)
 
-clf=KMeans(n_clusters=6,init="k-means++")
+clf=KMeans(n_clusters=3,init="k-means++")
 clf.fit(newlist)
 labels=clf.labels_
 print(labels)
 intlabels=[int(j) for j in labels]
 
-classify = {0: [], 1: [], 2: [],3:[],4:[],5:[]}
+classify = {0: [], 1: [],2:[]}
 num=0
 for i in intlabels:
     classify[i].append(model.wv.index_to_key[num])
